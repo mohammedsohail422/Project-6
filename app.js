93128e4a97241fdd619745ef4766a55b376599c4
sohail <mohammedsohail422@gmail.com>
2022-10-08 17:51:26 +0530

    sliders done

--- app.js
+++ app.js
@@ -2,42 +2,42 @@ let myObj = {
   products: [
     {
       ProductImage: "/Projects/Project-6/images/sliders/s4.jpg",
-      ProductName: "tv",
-    },
-    {
-      ProductImage: "/Projects/Project-6/images/sliders/s1.jpg",
-      ProductName: "watch",
-    },
-    {
-      ProductImage: "/Projects/Project-6/images/sliders/s1.jpg",
-      ProductName: "tv",
-    },
-    {
-      ProductImage: "/Projects/Project-6/images/sliders/s1.jpg",
-      ProductName: "watch",
-    },
-    {
-      ProductImage: "/Projects/Project-6/images/sliders/s1.jpg",
-      ProductName: "apple",
-    },
-    {
-      ProductImage: "/Projects/Project-6/images/sliders/s1.jpg",
-      ProductName: "tv",
-    },
-    {
-      ProductImage: "/Projects/Project-6/images/sliders/s3.jpg",
-      ProductName: "watch",
+      ProductName: "Highlight",
     },
     {
       ProductImage: "/Projects/Project-6/images/sliders/s2.jpg",
-      ProductName: "apple",
+      ProductName: "Vacation",
+    },
+    {
+      ProductImage: "/Projects/Project-6/images/sliders/s3.jpg",
+      ProductName: "London",
+    },
+    {
+      ProductImage: "/Projects/Project-6/images/sliders/s6.jpg",
+      ProductName: "USA",
+    },
+    {
+      ProductImage: "/Projects/Project-6/images/sliders/s5.jpg",
+      ProductName: "workshop",
     },
     {
       ProductImage: "/Projects/Project-6/images/sliders/s1.jpg",
-      ProductName: "watch",
+      ProductName: "office",
     },
     {
-      ProductImage: "/Projects/Project-6/images/sliders/s1.jpg",
+      ProductImage: "/Projects/Project-6/images/sliders/s3.jpg",
+      ProductName: "Banglore",
+    },
+    {
+      ProductImage: "/Projects/Project-6/images/sliders/s2.jpg",
+      ProductName: "Canada",
+    },
+    {
+      ProductImage: "/Projects/Project-6/images/sliders/s5.jpg",
+      ProductName: "Sucess",
+    },
+    {
+      ProductImage: "/Projects/Project-6/images/sliders/s4.jpg",
       ProductName: "apple",
     },
     {
@@ -58,7 +58,39 @@ let products = "";
 for (let x in myObj.products) {
   products += `<div class = "story-circle">`;
   products += `<img class="prod-img" src=${myObj.products[x].ProductImage} alt"image of product" ></img>`;
+  products += `<p>${myObj.products[x].ProductName}</p>`;
   products += `</div>`;
-  //   products += `<p>${myObj.products[x].ProductName}</p>`;
 }
 document.querySelector(".storys-container").innerHTML = products;
+
+let currentScrollPosition = 0;
+let scrollAmount = 320;
+
+const sCont = document.querySelector(".storys-container");
+const hScroll = document.querySelector(".horizontal-scroll");
+const btnScrollLeft = document.querySelector("#btn-scroll-left");
+const btnScrollRight = document.querySelector("#btn-scroll-right");
+
+btnScrollLeft.style.opacity = 0;
+
+let maxScroll = -sCont.offsetWidth + hScroll.offsetWidth;
+console.log(maxScroll);
+
+function scrollHorizontal(val) {
+  console.log(val);
+  currentScrollPosition += val * scrollAmount;
+  if (currentScrollPosition >= 0) {
+    currentScrollPosition = 0;
+    btnScrollLeft.style.opacity = 0;
+  } else {
+    btnScrollLeft.style.opacity = 1;
+  }
+  if (currentScrollPosition <= maxScroll) {
+    currentScrollPosition = maxScroll;
+    console.log(maxScroll);
+    btnScrollRight.style.opacity = 0;
+  } else {
+    btnScrollRight.style.opacity = 1;
+  }
+  sCont.style.left = currentScrollPosition + "px";
+}
